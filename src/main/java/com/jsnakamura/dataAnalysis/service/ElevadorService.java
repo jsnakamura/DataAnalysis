5b75f01f70c32a5d5f5f1ba5f47af5c3a2296c23
Jsnakamura <juliano.s.nakamura@hotmail.com>
2020-08-31 18:09:22 -0300

    Methods finished

--- src/main/java/com/jsnakamura/dataAnalysis/service/ElevadorService.java
+++ src/main/java/com/jsnakamura/dataAnalysis/service/ElevadorService.java
@@ -1,10 +1,9 @@
 package com.jsnakamura.dataAnalysis.service;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Collectors;
 
 import com.jsnakamura.dataAnalysis.entity.Answer;
 import com.jsnakamura.dataAnalysis.helper.DataHelper;
@@ -24,88 +23,246 @@ public class ElevadorService implements IElevadorService {
 
 		List<ArrayList<Answer>> listByFloors = dataHelper.getListByFloors(answers);
 
-		int size = 0;
-		
+		int count = 0;
+
 		for (List<Answer> floor : listByFloors) {
-			if(lessUsedFloor.isEmpty()) {
+			if (lessUsedFloor.isEmpty()) {
 				lessUsedFloor.add(floor.get(0).getAndar());
-				size = floor.size();
-				
-			} else if(floor.size() < size) {
+				count = floor.size();
+
+			} else if (floor.size() < count) {
 				lessUsedFloor.clear();
 				lessUsedFloor.add(floor.get(0).getAndar());
-				size = floor.size();
-				
-			} else if(floor.size() == size) {
+				count = floor.size();
+
+			} else if (floor.size() == count) {
 				lessUsedFloor.add(floor.get(0).getAndar());
 			}
 		}
-		
+
 		return lessUsedFloor;
 	}
 
 	public List<Character> elevadorMaisFrequentado() {
 
-		
+		List<Character> mostUsedElevator = new ArrayList<Character>();
 
-		return null;
+		List<ArrayList<Answer>> listByElevator = dataHelper.getListByElevators(answers);
+
+		int count = 0;
+
+		for (List<Answer> elevator : listByElevator) {
+			if (mostUsedElevator.isEmpty()) {
+				mostUsedElevator.add(elevator.get(0).getElevador());
+				count = elevator.size();
+
+			} else if (elevator.size() > count) {
+				mostUsedElevator.clear();
+				mostUsedElevator.add(elevator.get(0).getElevador());
+				count = elevator.size();
+
+			} else if (elevator.size() == count) {
+				mostUsedElevator.add(elevator.get(0).getElevador());
+			}
+		}
+
+		return mostUsedElevator;
 	}
 
 	public List<Character> periodoMaiorFluxoElevadorMaisFrequentado() {
 
-		List<Character> mostUsedElevatorBiggestFlow;
-		Map<Character, Integer> counter = new HashMap<Character, Integer>();
+		List<Character> mostUsedElevatorBiggestFlow = new ArrayList<Character>();
 
-		return null;
+		List<Character> mostUsedElevators = elevadorMaisFrequentado();
+
+		for (Character elevator : mostUsedElevators) {
+
+			List<Answer> completeMostUsedElevators;
+
+			int morningShift = 0;
+			int afternoonShift = 0;
+			int eveningShift = 0;
+
+			completeMostUsedElevators = answers.stream().filter(answer -> answer.getElevador() == elevator)
+					.collect(Collectors.toList());
+
+			for (Answer answer : completeMostUsedElevators) {
+
+				if (answer.getTurno() == 'M') {
+					morningShift++;
+				} else if (answer.getTurno() == 'V') {
+					afternoonShift++;
+				} else if (answer.getTurno() == 'N') {
+					eveningShift++;
+				}
+			}
+
+			if ((morningShift >= afternoonShift) && (morningShift >= eveningShift)) {
+				mostUsedElevatorBiggestFlow.add('M');
+
+			} else if ((afternoonShift >= morningShift) && (afternoonShift >= eveningShift)) {
+				mostUsedElevatorBiggestFlow.add('V');
+
+			} else if ((eveningShift >= morningShift) && (eveningShift >= afternoonShift)) {
+				mostUsedElevatorBiggestFlow.add('N');
+			}
+		}
+
+		return mostUsedElevatorBiggestFlow;
 	}
 
 	public List<Character> elevadorMenosFrequentado() {
 
-		List<Character> leastUsedElevator;
-		Map<Character, Integer> counter = new HashMap<Character, Integer>();
+		List<Character> leastUsedElevator = new ArrayList<Character>();
 
-		return null;
+		List<ArrayList<Answer>> listByElevator = dataHelper.getListByElevators(answers);
+
+		int count = 0;
+
+		for (List<Answer> elevator : listByElevator) {
+			if (leastUsedElevator.isEmpty()) {
+				leastUsedElevator.add(elevator.get(0).getElevador());
+				count = elevator.size();
+
+			} else if (elevator.size() < count) {
+				leastUsedElevator.clear();
+				leastUsedElevator.add(elevator.get(0).getElevador());
+				count = elevator.size();
+
+			} else if (elevator.size() == count) {
+				leastUsedElevator.add(elevator.get(0).getElevador());
+			}
+		}
+
+		return leastUsedElevator;
 	}
 
 	public List<Character> periodoMenorFluxoElevadorMenosFrequentado() {
 
-		List<Character> leastUsedElevatorLowestFlow;
-		Map<Character, Integer> counter = new HashMap<Character, Integer>();
+		List<Character> leastUsedElevatorLowestFlow = new ArrayList<Character>();
 
-		return null;
+		List<Character> leastUsedElevators = elevadorMenosFrequentado();
+
+		for (Character elevator : leastUsedElevators) {
+
+			List<Answer> completeLeastUsedElevators;
+
+			int morningShift = 0;
+			int afternoonShift = 0;
+			int eveningShift = 0;
+
+			completeLeastUsedElevators = answers.stream().filter(answer -> answer.getElevador() == elevator)
+					.collect(Collectors.toList());
+
+			for (Answer answer : completeLeastUsedElevators) {
+
+				if (answer.getTurno() == 'M') {
+					morningShift--;
+				} else if (answer.getTurno() == 'V') {
+					afternoonShift--;
+				} else if (answer.getTurno() == 'N') {
+					eveningShift--;
+				}
+			}
+
+			if ((morningShift >= afternoonShift) && (morningShift >= eveningShift)) {
+				leastUsedElevatorLowestFlow.add('M');
+
+			} else if ((afternoonShift >= morningShift) && (afternoonShift >= eveningShift)) {
+				leastUsedElevatorLowestFlow.add('V');
+
+			} else if ((eveningShift >= morningShift) && (eveningShift >= afternoonShift)) {
+				leastUsedElevatorLowestFlow.add('N');
+			}
+
+		}
+
+		return leastUsedElevatorLowestFlow;
 	}
 
 	public List<Character> periodoMaiorUtilizacaoConjuntoElevadores() {
 
-		List<Character> BiggestFlowElevatorSet;
-		Map<Character, Integer> counter = new HashMap<Character, Integer>();
+		List<Character> elevatorsBiggestFlow = new ArrayList<Character>();
 
-		return null;
+		List<ArrayList<Answer>> listByShifts = dataHelper.getListByShifts(answers);
+		
+		int count = 0;
+
+		for (List<Answer> shifts : listByShifts) {
+			if (elevatorsBiggestFlow.isEmpty()) {
+				elevatorsBiggestFlow.add(shifts.get(0).getTurno());
+				count = shifts.size();
+
+			} else if (shifts.size() > count) {
+				elevatorsBiggestFlow.clear();
+				elevatorsBiggestFlow.add(shifts.get(0).getTurno());
+				count = shifts.size();
+
+			} else if (shifts.size() == count) {
+				elevatorsBiggestFlow.add(shifts.get(0).getTurno());
+			}
+		}
+		
+		return elevatorsBiggestFlow;
 	}
 
 	public float percentualDeUsoElevadorA() {
 
-		return 0;
+		float total;
+		float elevatorAUsage;
+		
+		total = answers.size();
+		
+		elevatorAUsage = answers.stream().filter(answer -> answer.getElevador() == 'A').count();
+		
+		return Float.parseFloat(new DecimalFormat("#,##0.00").format(elevatorAUsage/total));
 	}
 
 	public float percentualDeUsoElevadorB() {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		float total;
+		float elevatorBUsage;
+		
+		total = answers.size();
+		
+		elevatorBUsage = answers.stream().filter(answer -> answer.getElevador() == 'B').count();
+		
+		return Float.parseFloat(new DecimalFormat("#,##0.00").format(elevatorBUsage/total));
 	}
 
 	public float percentualDeUsoElevadorC() {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		float total;
+		float elevatorCUsage;
+		
+		total = answers.size();
+		
+		elevatorCUsage = answers.stream().filter(answer -> answer.getElevador() == 'C').count();
+		
+		return Float.parseFloat(new DecimalFormat("#,##0.00").format(elevatorCUsage/total));
 	}
 
 	public float percentualDeUsoElevadorD() {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		float total;
+		float elevatorDUsage;
+		
+		total = answers.size();
+		
+		elevatorDUsage = answers.stream().filter(answer -> answer.getElevador() == 'D').count();
+		
+		return Float.parseFloat(new DecimalFormat("#,##0.00").format(elevatorDUsage/total));
 	}
 
 	public float percentualDeUsoElevadorE() {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		float total;
+		float elevatorEUsage;
+		
+		total = answers.size();
+		
+		elevatorEUsage = answers.stream().filter(answer -> answer.getElevador() == 'E').count();
+		
+		return Float.parseFloat(new DecimalFormat("#,##0.00").format(elevatorEUsage/total));
 	}
-
 }
